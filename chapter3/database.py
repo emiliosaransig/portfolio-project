e517d6c97e5072bf89ac619c8d50b7afb1c80123
emiliosaransig <emiliowille@outlook.de>
2026-05-15 16:24:51 +0200

    fixed SQLALCHEMY_DATABASE_URL

--- chapter3/database.py
+++ chapter3/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
